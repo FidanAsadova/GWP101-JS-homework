cbb425dd8c3ff642be748706c3bf569a22acd1c5
FidanAsadova <7ksn2xn@code.edu.az>
2023-04-08 23:42:30 +0400

    14.Loop-for-while-and-if-else folder added

---
+++ 14.Loop-for-while-and-if-else-statement/scrtip.js
@@ -0,0 +1,27 @@
+// // 1.Convert Age to Days
+
+function Yearof(age) {
+  return age * 365;
+}
+console.log(Yearof(65));
+console.log(Yearof(0));
+console.log(Yearof(20));
+
+
+
+
+
+
+// 1.Loop text
+let myName = "Front-end Developer";
+for(let i = 0; i < myName.length; i++){
+    console.log(myName[i])
+}
+// let myName1 = "Fr";
+// for(let i = 0; i < myName1.length; i++){
+//     console.log(myName1)
+// }
+// let myName2 = "Fro";
+// for(let i = 0; i < myName2.length; i++){
+//     console.log(myName2)
+// }
