43080a95475e17152b2b5d189daf8b1cd579cc53
FidanAsadova <7ksn2xn@code.edu.az>
2023-04-30 14:55:55 +0400

    Simple Calculator folder added

---
+++ 20.Simple-Calculator/script.js
@@ -0,0 +1,51 @@
+let form = document.querySelector("#form");
+let bgBlack = document.querySelector("#blackBG");
+let value1 = document.querySelector("#value1");
+let value2 = document.querySelector("#value2");
+let additionBtn = document.querySelector("#addition");
+let substractBtn = document.querySelector("#substract");
+let multiplyBtn = document.querySelector("#multiply");
+let divisionBtn = document.querySelector("#division");
+let resetBtn = document.querySelector("#reset");
+
+typeof Number(value1.value);
+typeof Number(value2.value);
+typeof Number(bgBlack);
+
+additionBtn.addEventListener("click", function () {
+  if (value1.value && value2.value) {
+    let add = value1.value + value2.value;
+    bgBlack.innerHTML = add;
+  } else {
+    alert("Value is empty");
+  }
+});
+substractBtn.addEventListener("click", function () {
+  if (value1.value && value2.value) {
+    let sub = value1.value - value2.value;
+    bgBlack.innerHTML = sub;
+  } else {
+    alert("Value is empty");
+  }
+});
+multiplyBtn.addEventListener("click", function () {
+  if (value1.value && value2.value) {
+    let multy = value1.value * value2.value;
+    bgBlack.innerHTML = multy;
+  } else {
+    alert("Value is empty");
+  }
+});
+divisionBtn.addEventListener("click", function () {
+  if (value1.value && value2.value) {
+    let divis = value1.value / value2.value;
+    bgBlack.innerHTML = divis;
+  } else {
+    alert("Value is empty");
+  }
+});
+resetBtn.addEventListener("click", function () {
+  value1.value = "";
+  value2.value = "";
+  bgBlack.innerHTML = 0;
+});
